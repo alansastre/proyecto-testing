2542cba2872a95baef6a8a6df605555e22e7b0ad
alansastre <alan@alansastre.co>
2021-11-18 10:25:09 +0100

    integrate selenium - github actions

---
+++ src/test/java/com/example/proyectotesting/selenium/ExampleTest.java
@@ -0,0 +1,51 @@
+package com.example.proyectotesting.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    // Navegador
+    WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+//        String dir = System.getProperty("user.dir"); // ruta del proyecto
+//        String driverUrl = "/drivers/chromedriver.exe";
+//        String url = dir + driverUrl;
+//        System.setProperty("webdriver.chrome.driver", url);
+//        driver = new ChromeDriver(); // Google Chrome
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    void tagNameSelector(){
+        driver.get("https://github.com/mozilla");
+
+        WebElement h1 = driver.findElement(By.tagName("h1"));
+        String h1Text = h1.getText();
+        assertEquals("Mozilla", h1Text);
+    }
+
+}
