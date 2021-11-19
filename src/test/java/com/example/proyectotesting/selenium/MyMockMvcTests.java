4652798afe8947ac2f96a1c266949d460a22f499
alansastre <alan@alansastre.co>
2021-11-19 10:49:47 +0100

    Spring Test MVC controllers

--- src/test/java/com/example/proyectotesting/selenium/MyMockMvcTests.java
+++ src/test/java/com/example/proyectotesting/selenium/MyMockMvcTests.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
@@ -21,11 +23,21 @@ class MyMockMvcTests {
     MockMvc mvc;
 
     @Test
-    void exampleTest() throws Exception {
+    void obtenerListaTest() throws Exception {
+
         mvc.perform(get("/products")) // url a testear: http://localhost:8080/products
                 .andExpect(status().isOk()) // estado HTTP de la respuesta 200
                 .andExpect(model().attributeExists("products")) // comprobar los atributos cargados en el modelo
+                .andExpect(view().name("product-list")) // comprobar los atributos cargados en el modelo
                 .andExpect( forwardedUrl("/WEB-INF/views/product-list.jsp")); // vista que se mostrará
+
     }
 
+    @Test
+    void crearProductoTest() throws Exception {
+        mvc.perform(
+                    post("/products").param("name", "producto prueba")
+                ).andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/products"));
+    }
 }
