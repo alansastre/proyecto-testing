a4ddbd17951258d56a11a78355c882b25eb733b6
alansastre <alan@alansastre.co>
2021-11-19 09:18:19 +0100

    print env properties

--- src/test/java/com/example/proyectotesting/selenium/ExampleTest.java
+++ src/test/java/com/example/proyectotesting/selenium/ExampleTest.java
@@ -9,9 +9,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 public class ExampleTest {
 
     // Navegador
@@ -24,6 +26,9 @@ public class ExampleTest {
 //        String url = dir + driverUrl;
 //        System.setProperty("webdriver.chrome.driver", url);
 //        driver = new ChromeDriver(); // Google Chrome
+
+        System.getenv().forEach((key, value) -> System.out.println(key + " " + value));
+        System.getProperties().forEach((key, value) -> System.out.println(key + " " + value));
 
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
@@ -43,6 +48,7 @@ public class ExampleTest {
     void tagNameSelector(){
 //        driver.get("https://github.com/mozilla");
         driver.get("https://proyecto-testing.herokuapp.com");
+//        driver.get("http://localhost:8080/");
 
         WebElement h1 = driver.findElement(By.tagName("h1"));
         String h1Text = h1.getText();
